6516a493c7525c85142e320ee175a90fdf6c1de0
Matthew Eckes <eckesm@gmail.com>
2021-09-25 22:39:58 -0400

    Completed assignment.

--- graph.js
+++ graph.js
@@ -1,35 +1,79 @@
 class Node {
-  constructor(value, adjacent = new Set()) {
-    this.value = value;
-    this.adjacent = adjacent;
-  }
+	constructor(value, adjacent = new Set()) {
+		this.value = value;
+		this.adjacent = adjacent;
+	}
 }
 
 class Graph {
-  constructor() {
-    this.nodes = new Set();
-  }
+	constructor() {
+		this.nodes = new Set();
+	}
 
-  // this function accepts a Node instance and adds it to the nodes property on the graph
-  addVertex(vertex) {}
+	// this function accepts a Node instance and adds it to the nodes property on the graph
+	addVertex(vertex) {
+		this.nodes.add(vertex);
+	}
 
-  // this function accepts an array of Node instances and adds them to the nodes property on the graph
-  addVertices(vertexArray) {}
+	// this function accepts an array of Node instances and adds them to the nodes property on the graph
+	addVertices(vertexArray) {
+		for (let node of vertexArray) {
+			this.addVertex(node);
+		}
+	}
 
-  // this function accepts two vertices and updates their adjacent values to include the other vertex
-  addEdge(v1, v2) {}
+	// this function accepts two vertices and updates their adjacent values to include the other vertex
+	addEdge(v1, v2) {
+		v1.adjacent.add(v2);
+		v2.adjacent.add(v1);
+	}
 
-  // this function accepts two vertices and updates their adjacent values to remove the other vertex
-  removeEdge(v1, v2) {}
+	// this function accepts two vertices and updates their adjacent values to remove the other vertex
+	removeEdge(v1, v2) {
+		v1.adjacent.delete(v2);
+		v2.adjacent.delete(v1);
+	}
 
-  // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
-  removeVertex(vertex) {}
+	// this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
+	removeVertex(vertex) {
+		this.nodes.delete(vertex);
+	}
 
-  // this function returns an array of Node values using DFS
-  depthFirstSearch(start) {}
+	// this function returns an array of Node values using DFS
+	depthFirstSearch(start) {
+		let visited = [];
+		let toVisitStack = [ start ];
+		let seen = new Set(toVisitStack);
+		while (toVisitStack.length) {
+			let currNode = toVisitStack.pop();
+			visited.push(currNode.value);
+			for (let connectedNode of currNode.adjacent) {
+				if (!seen.has(connectedNode)) {
+					toVisitStack.push(connectedNode);
+					seen.add(connectedNode);
+				}
+			}
+		}
+		return visited;
+	}
 
-  // this function returns an array of Node values using BFS
-  breadthFirstSearch(start) {}
+	// this function returns an array of Node values using BFS
+	breadthFirstSearch(start) {
+		let visited = [];
+		let toVisitQueue = [ start ];
+		let seen = new Set(toVisitQueue);
+		while (toVisitQueue.length) {
+			let currNode = toVisitQueue.shift();
+			visited.push(currNode.value);
+			for (let connectedNode of currNode.adjacent) {
+				if (!seen.has(connectedNode)) {
+					toVisitQueue.push(connectedNode);
+					seen.add(connectedNode);
+				}
+			}
+		}
+		return visited;
+	}
 }
 
-module.exports = {Graph, Node}
+module.exports = { Graph, Node };
